259697bac85082d0b7642fb55a945bfef1c4c93c
chaidenfoanto <148606330+chaidenfoanto@users.noreply.github.com>
2024-11-09 12:51:52 +0800

    Update journal.php

--- blog/app/Models/journal.php
+++ blog/app/Models/journal.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class journal extends Model
 {
     use HasFactory;
 
